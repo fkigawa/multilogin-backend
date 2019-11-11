0a336f15a8ad44959584c40fc08c00d39af9b7c1
Francis Kigawa <fkigawa@Franciss-MBP.wireless.yale.internal>
2019-11-10 20:08:15 -0500

    first commit

---
+++ routes/api/companies.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const passport = require('passport');
+const router = require('express').Router();
+const auth = require('../auth');
+
+router.put('/:company', (req, res) => {
+
+})
+
+
+router.get('/:company', (req, res) => {
+
+})
+
+
+router.delete('/:company', (req, res) => {
+
+})
+
+module.exports = router;
